5d3e3cae027f0896d1ce98df787a7925bf94926e
Matthew Law <matthew.law@virginmediao2.co.uk>
2023-03-03 10:40:43 +0000

    Initial commit

---
+++ apps/toolkit-demos/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-toolkit-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'toolkit-demos';
+}
