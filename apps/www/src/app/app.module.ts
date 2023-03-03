286ad67de9ac8e51e8adec4f66c52caf4a0b0fad
Jim Drury <jim.drury@virginmediao2.co.uk>
2023-03-03 14:31:20 +0000

    feat(www): adds angular universal

--- apps/www/src/app/app.module.ts
+++ apps/www/src/app/app.module.ts
@@ -1,18 +1,17 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import {AppComponent} from './app.component';
-import {appRoutes} from './app.routes';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes, {initialNavigation: 'enabledBlocking'}),
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
